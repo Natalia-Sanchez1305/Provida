07fd02276c3c69fa3748e2ef28c7aa2baab942a1
HDFLAPSANCHEZNS\sanchezns <natalia.sanchez-sanchez@metlife.com.mx>
2021-04-14 20:51:12 -0500

    prueba

--- ProvidaPOM/src/main/java/com/pageobjects/SearchProvidaPage.java
+++ ProvidaPOM/src/main/java/com/pageobjects/SearchProvidaPage.java
@@ -27,8 +27,29 @@ public class SearchProvidaPage extends Base {
 	By domain = By.id("DOMAIN");
 	By login = By.id("SignInButtonText");
 	
-		
-		//public void SearchProvida throws InterruptedException {
+	By btnGenerales = By.cssSelector("#root > div.container > div > div > div:nth-child(3) > div > div.card-footer.border-0.pb-4 > a");
+	
+	By btnConsultador = By.cssSelector("#root > div.container > div > div > div:nth-child(1) > div > div.card-footer.border-0.pb-4 > a");
+	
+	By txtPoliza = By.name("Poliza");
+	By btnBuscar = By.cssSelector("#root > div.container > div > div > form > div:nth-child(5) > div:nth-child(1) > button");
+	
+	By lnkNumPoliza = By.cssSelector("#root > div.container > div > div.ml-table-container > div.table-responsive-sm > table > tbody > tr > td:nth-child(1) > a");
+	
+	By InfoGenral = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(1) > a");
+	
+	By DatosContacto = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(2) > a");
+	
+	By Coberturas = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(3) > a");
+	
+	By Beneficiarios = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(4) > a");
+	
+	By Servicios = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(5) > a");	
+	
+	By Agente = By.cssSelector("#root > div.container > div.fixed-bottom.ml-table-container > div > table > tbody > tr > td:nth-child(5) > a");
+	
+	
+	//public void SearchProvida throws InterruptedException {
 			//ExcelDataProvider datos = new ExcelDataProvider("./data/PROVIDA.xls");
 			//for(int i = 0;i < datos.getNumRows();i++) {
 				//datos.setRow(i);
@@ -47,7 +68,49 @@ public class SearchProvidaPage extends Base {
 			type("ALICOCORP", domain);
 			click(login);
 		}
-			}
+		
+		public void MenuGenerales() {
+			click(btnGenerales);
+		}
+		
+		public void SubmenuConsultador() {
+			click(btnConsultador);
+		}
+		
+		public void typePoliza() {
+			type("PVN456",txtPoliza);
+		}
+		
+		public void BuscarPoliza() {
+			click(btnBuscar);
+		}
+		
+		public void lnkPoliza() {
+			click(lnkNumPoliza);
+		}
+		
+		public void InfoGeneral() {
+			click(InfoGenral);
+		}
+		
+		public void DatosContacto() {
+			click(DatosContacto);
+		}
+		
+		public void Coberturas() {
+			click(Coberturas);
+		}
+		public void Beneficiarios() {
+			click(Beneficiarios);
+		}
+		public void Servicios() {
+			click(Servicios);
+		}
+		public void Agente() {
+			click(Agente);
+		}
+
+}
 
 
 
